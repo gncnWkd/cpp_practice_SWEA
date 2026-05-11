e602bbd39c02d8e5a2cf20f6076eb431dc82b1e5
전현찬 <2020014975@hanyang.ac.kr>
2026-05-11 12:07:15 +0900

    1869 solved

--- Project1/1859.cpp
+++ Project1/1859.cpp
@@ -1,77 +1,47 @@
-#include "iostream"
-#include "vector"
-#include "sstream"
-#include "string"
-
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 int main() {
-	int T, N;
+	int T, N, price;
 	cin >> T;
-	string line;
-	
+	vector<int> prices;
 
 	for (int i = 0; i < T; i++) {
 		cin >> N;
-		cin.ignore();
-		vector<int> prices;
-
-		getline(cin, line);
-
-		istringstream iss(line);
-		string word;
-
-		while (iss >> word) {
-			prices.push_back(stoi(word));
-		}
 		
-		// 최대 이익을 계산
-		// 가격이 오르다 떨어지기 시작하면, 떨어지기 직전에 파는 것이 이득
-		// 반드시 가격이 한번 이상 올라야 팔수있음
-		// 오르기만하면 마지막에 팔아야 하고, 떨어지기만하면 팔 수 없음
-		// 가격의 흐름이 오름을 한번 이상 포함한 오름,유지의 연속 => 하락인경우 하락 직전의 가격에서 팔기
-		// is_rising이 true, is_falling이 false인 상태에서 is_falling이 true가 되면 판매하고 is_rising을 false로 바꿈
-		// is_rising이 false, is_falling이 true인 상태에서 is_rising이 true가 되면 if_falling을 false로 바꿈
+		for (int j = 0; j < N; j++) {
+			cin >> price;
+			prices.push_back(price);
+		}
+		// 1 1 4 3 6
+				// 6 2 3 4 1
+				// 1 4 6 3 1
+		//1 4 6 1 3
+		// 1 4 6 1 3 3
 
+		//뒤에서 시작해서 pivot을 맨마지막으로 주고, 하나씩 앞으로 가면서 차이가 0보다 크면 합산, 0보다 작거나 같으면 pivot 변경
 
-		bool is_rising, is_falling = false;
-		int diff = 0;
-		int sell_pivot = 0;
+		int pivot = N-1;
+
+		//sum이 최대 100만개의 날짜 x 매매가의 최대값이 9999원이므로 99990000000는 int범위로 커버할 수 없음
+		long sum = 0;
+		for (int j = N-2; j >= 0; j--) {
+			if (prices[pivot] - prices[j] > 0) {
+				sum += prices[pivot] - prices[j];
+			}
+			else {
+				pivot = j;
+			}
+		}
+
+		cout << "#" << i + 1 << " " << sum << endl;
 		
-		for (int i = 1; i < prices.size(); i++) {
-			diff = prices[i] - prices[i - 1];
-			
-			if (is_rising == true && diff < 0) {
-				//sell
-				is_rising = false;
-				is_falling = true;
-			}
-			else if (is_falling == true && diff > 0) {
-				is_falling = false;
-				is_rising = true;
-			}
-
-			else if (diff < 0) {
-				is_falling = true;
-			}
-			else if (diff > 0) {
-				is_rising = true;
-			}
-		}
-
-		if (sell_pivot == 0 && is_rising == true) {
-			//sell in the end
-		}
-
-
-
-
-
-
-
-
 
 		prices.clear();
 	}
-	
+
+
+	return 0;
 }
